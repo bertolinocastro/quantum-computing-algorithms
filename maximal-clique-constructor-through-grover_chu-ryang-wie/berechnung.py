6fe4cc13a2a4c5c278683ff9f2cc7eeea4c0f741
bertolinocastro <jgabbc@hotmail.com>
2020-02-20 02:57:11 -0300

    getting used to the lib

--- maximal-clique-constructor-through-grover_chu-ryang-wie/berechnung.py
+++ maximal-clique-constructor-through-grover_chu-ryang-wie/berechnung.py
@@ -1,4 +1,24 @@
 from sympy import *
+#import sympy.physics.quantum as qp # quantum physics
+from sympy.physics.quantum.qubit import *
+
+# base network G:
+# 1 <-> 2 <-> 3
+# Nodes: 1, 2, 3
+# Edges: (1,2), (2,3)
+
+# Adjacency matrix for network G
+A = Matrix([
+	[0, 1, 0],
+	[1, 0, 1],
+	[0, 1, 0]
+])
 
 
+# creating qubits for n=3 basis
+q = [Qubit(f'{dummy:03b}') for dummy in range(2**3)]
+pprint(q)
+
+# step: constructing the U operator basics using a network with n=3
+# function to represent qubit as matrix: qubit_to_matrix()
 
